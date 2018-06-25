9ed0b63597cb08a5a492f81420e7d3ed7a23b09b
Goran Radosevic <goran.radosevic@outlook.com>
2018-06-25 21:28:34 +0200

    laravel passport

--- resources/assets/js/app.js
+++ resources/assets/js/app.js
@@ -7,6 +7,13 @@
 
 require('./bootstrap');
 
+window.axios = require('axios');
+
+window.axios.defaults.headers.common = {
+    'X-CSRF-TOKEN': window.Laravel.csrfToken,
+    'X-Requested-With': 'XMLHttpRequest'
+};
+
 window.Vue = require('vue');
 
 /**
@@ -16,6 +23,9 @@ window.Vue = require('vue');
  */
 
 Vue.component('example-component', require('./components/ExampleComponent.vue'));
+Vue.component('passport-clients', require('./components/passport/Clients.vue'));
+Vue.component('passport-authorized-clients', require('./components/passport/AuthorizedClients.vue'));
+Vue.component('passport-personal-access-tokens', require('./components/passport/PersonalAccessTokens.vue'));
 
 const app = new Vue({
     el: '#app'
---
+++ resources/views/home.blade.php
@@ -0,0 +1,37 @@
+@extends('layouts.app')
+
+@section('content')
+<div class="container">
+    <div class="row justify-content-center">
+        <div class="col-md-8">
+            <div class="card">
+                <div class="card-header">Dashboard</div>
+
+                <div class="card-body">
+                    @if (session('status'))
+                        <div class="alert alert-success" role="alert">
+                            {{ session('status') }}
+                        </div>
+                    @endif
+                </div>
+            </div>
+            <passport-personal-access-tokens></passport-personal-access-tokens>
+        </div>
+    </div>
+</div>
+@endsection
+
+@section('scripts')
+    <script
+            src="https://code.jquery.com/jquery-2.2.4.min.js"
+            integrity="sha256-BbhdlvQf/xTY9gja0Dq3HiwQF8LaCRTXxZKRutelT44="
+            crossorigin="anonymous"></script>
+    <script type="text/javascript">
+        setTimeout(function() {
+            window.axios.get('/api/user')
+                .then(function (response) {
+                    console.log(response.data);
+                });
+        },1000);
+    </script>
+@endsection
