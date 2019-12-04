586d4defd6b723e63e63f5ad2e4fc817a4768bf9
Muhamed Didovic <muhamed.didovic@gmail.com>
2019-12-04 10:27:40 +0100

    trmstrings

--- app/Http/Middleware/TrimStrings.php
+++ app/Http/Middleware/TrimStrings.php
@@ -1,5 +1,4 @@
-<?php
-namespace App\Http\Middleware;
+<?php namespace App\Http\Middleware;
 use Illuminate\Foundation\Http\Middleware\TrimStrings as Middleware;
 
 class TrimStrings extends Middleware
