b464b39d432796a85f00516ae0700846a1653970
Muhamed Didovic <muhamed.didovic@gmail.com>
2019-12-12 15:16:19 +0100

    add two spaces for Encrypt cookies

--- app/Http/Middleware/EncryptCookies.php
+++ app/Http/Middleware/EncryptCookies.php
@@ -2,6 +2,8 @@
 use Illuminate\Cookie\Middleware\EncryptCookies as Middleware;
 
 class EncryptCookies extends Middleware
+
+
 {
     /**
      * The names of the cookies that should not be encrypted.
